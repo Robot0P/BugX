6bc61d0d53a99a19bbd194cc6bb40784d93452a6
Robot0P <65432427+Robot0P@users.noreply.github.com>
2023-11-06 16:04:37 +0800

    Update dashboard.tsx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -251,7 +251,7 @@ export default function DashBoard({ data }: { data: DataItem }) {
         }}
         loading={menuLoadding}
         menu={{ request: async () => getMenuData() }}
-        title="One XSS 管理平台"
+        title="FromXS"
         logo="/logo.png"
         avatarProps={{
           // icon: <UserOutlined rev={undefined} />,
